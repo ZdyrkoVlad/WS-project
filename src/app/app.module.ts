4f723562a86cb332410fc8dc5ad1c3f4446468ef
carma51 <carma51beaf@gmail.com>
2022-12-18 15:47:09 +0200

    assets path uses custom factory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,8 +30,8 @@ export function HttpLoaderFactory (http: HttpClient) {
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient],
       }
     })
   ],
@@ -39,3 +39,7 @@ export function HttpLoaderFactory (http: HttpClient) {
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
